35ab5e931f8348d05cdd80b2ffb5b055e68dabf9
Stamatis Katsaounis <katsaouniss@gmail.com>
2019-10-14 09:52:48 +0300

    Update Licences
    Signed-off-by: Stamatis Katsaounis <katsaouniss@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2019  Stamatis Katsaounis <skatsaounis@admin.grnet.gr>
+# Copyright (C) 2019  GRNET S.A.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
